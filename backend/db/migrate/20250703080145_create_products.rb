8b613741d56388cda4c4415010fe7614b0864737
felix1251 <felixabacajen17@gmail.com>
2025-07-03 16:06:53 +0800

    feat: product scaffold

--- backend/db/migrate/20250703080145_create_products.rb
+++ backend/db/migrate/20250703080145_create_products.rb
@@ -2,8 +2,8 @@ class CreateProducts < ActiveRecord::Migration[7.2]
   def change
     create_table :products do |t|
       t.string :title, null: false
-      t.text :description
-      t.boolean :with_variants, default: false
+      t.text :description, default: ""
+      t.boolean :with_variants, null: false, default: false
       t.string :currency, null: false, default: "php"
 
       t.timestamps
